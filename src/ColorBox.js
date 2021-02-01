79462bb9b955d894bca0965cb53762cd92ef290d
danajackson2 <danajackson2@gmail.com>
2021-02-01 15:20:15 -0800

    beautiful boxes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,17 +4,15 @@ export default class ColorBox extends Component {
 
   state = {
     todos: [
-      
     ]
   }
 
   render() {
+    const op = this.props.opacity
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className ='color-box' style={{opacity:op}}>
+        {op >= 0.2 ? <ColorBox opacity={op  - 0.1} /> : null}
       </div>
     )
   }
-
 }
-
